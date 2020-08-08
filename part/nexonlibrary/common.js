250d0e088efebd8a66693c2c98657f18c81c82b9
Kimbangul <highcolor_12@naver.com>
2020-08-09 03:34:24 +0900

    메인 슬라이드 작업 1

--- part/nexonlibrary/common.js
+++ part/nexonlibrary/common.js
@@ -23,7 +23,6 @@ menuItemHasSubMenu.hover(
     slideDown, slideUp
 );
 }
-
 function mbGnb_init(){
     var openBtn = $('header > nav.mb > div.menu-btn');
     var closeBtn = $('header > nav.mb > div.gnb > div.menu > div.menu-top > div.close-btn');
@@ -45,6 +44,20 @@ function mbGnb_init(){
 
 
 }
+
+// slick slide
+$('.slider-1').slick({
+    slide: 'li',
+    dots : true,
+    infinite : true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    pauseOnHover : false,
+    arrows: true,
+    prevArrow : "<button type='button' class='slick-prev'>Previous</button>",		// 이전 화살표 모양 설정
+	nextArrow : "<button type='button' class='slick-next'>Next</button>"		// 다음 화살표 모양 설정
+ });
+
 
 
 
