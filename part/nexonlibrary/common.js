034b3c8ba296c58e0f04895fda17e0320024fa0f
Kimbangul <highcolor_12@naver.com>
2020-08-09 01:36:42 +0900

    모바일 탑바 작업

--- part/nexonlibrary/common.js
+++ part/nexonlibrary/common.js
@@ -4,7 +4,9 @@
 $(document).ready(function(){
 
     // header
-var menuItemHasSubMenu = $('header > nav > ul.menu > li').has('ul.submenu');
+
+function pcGnb_init(){
+    var menuItemHasSubMenu = $('header > nav > ul.menu > li').has('ul.submenu');
 
 function slideDown(){
 
@@ -20,6 +22,34 @@ function slideUp(){
 menuItemHasSubMenu.hover(
     slideDown, slideUp
 );
+}
+
+function mbGnb_init(){
+    var openBtn = $('header > nav.mb > div.menu-btn');
+    var closeBtn = $('header > nav.mb > div.gnb > div.menu > div.menu-top > div.close-btn');
+    var mbGnb = $('header > nav.mb > div.gnb');
+
+    function mbGnbOpen(){        
+        mbGnb.addClass('on');
+    }
+
+    
+    function mbGnbClose(){    
+        mbGnb.removeClass('on');
+    }
+    
+
+
+    openBtn.click(mbGnbOpen);
+    closeBtn.click(mbGnbClose);
+
+
+}
+
+
+
+pcGnb_init();
+mbGnb_init();
 
 
 
